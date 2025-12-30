d3325badbb71f7f904700b530c73ec494491bbba
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2025-12-30 15:25:47 +0530

    completed

---
+++ jenkins/basic-security.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin123")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
+
+Jenkins.instance.getSetupWizard().completeSetup()
